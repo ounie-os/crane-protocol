950c6cbc8fd0127da0c9841de74ee40f145c4295
ounie_os <ounie@qq.com>
2020-10-26 17:09:55 +0800

    decode在http的长连接中会被调用。根据设备注册状态选择返回注册信息还是属性信息。

--- src/main/java/org/company/protocol/crane/CraneDeviceMessageCodec.java
+++ src/main/java/org/company/protocol/crane/CraneDeviceMessageCodec.java
@@ -1,25 +1,30 @@
 package org.company.protocol.crane;
 
+import com.alibaba.fastjson.JSONObject;
 import io.netty.buffer.Unpooled;
 import lombok.AllArgsConstructor;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
+import org.jetlinks.core.device.DeviceRegistry;
+import org.jetlinks.core.message.DeviceRegisterMessage;
 import org.jetlinks.core.message.Message;
 import org.jetlinks.core.message.codec.*;
+import org.jetlinks.core.message.codec.http.HttpExchangeMessage;
 import org.jetlinks.core.message.codec.http.SimpleHttpResponseMessage;
 import org.reactivestreams.Publisher;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import reactor.core.publisher.Mono;
+
+import java.util.Set;
 
 @AllArgsConstructor
+@Setter
 @Slf4j(topic = "system.crane")
-public class CraneDeviceMessageCodec implements DeviceMessageCodec{
+public class CraneDeviceMessageCodec extends UrlTopicMessageCodec implements DeviceMessageCodec{
 
-    private static final SimpleHttpResponseMessage response = SimpleHttpResponseMessage
-                    .builder()
-                    .payload(Unpooled.wrappedBuffer("{success:true}".getBytes()))
-            .contentType(MediaType.APPLICATION_JSON)
-            .status(200)
-            .build();
+    private DeviceRegistry registry;
 
     @Override
     public Transport getSupportTransport() {
@@ -29,14 +34,49 @@ public class CraneDeviceMessageCodec implements DeviceMessageCodec{
     @NotNull
     @Override
     public Publisher<? extends Message> decode(@NotNull MessageDecodeContext messageDecodeContext) {
-        log.info("decode");
-        return null;
+        HttpExchangeMessage message = (HttpExchangeMessage) messageDecodeContext.getMessage();
+        String url = message.getUrl();
+        JSONObject payload = message.payloadAsJson();
+        String deviceId = payload.getString("imei");
+
+        return Mono.defer(() -> {
+            return registry.getDevice(deviceId)
+                            .flatMap(operator -> {
+                            return Mono.justOrEmpty(doDecode(null, url, payload))
+                                    .switchIfEmpty(Mono.defer(() -> {
+                                        //未转换成功，响应404
+                                        return message.response(SimpleHttpResponseMessage
+                                                .builder()
+                                                .status(404)
+                                                .contentType(MediaType.APPLICATION_JSON)
+                                                .payload(Unpooled.wrappedBuffer("{\"success\":false}".getBytes()))
+                                                .build()).then(Mono.empty());
+                                    }))
+                                    .flatMap(msg -> {
+                                        //响应成功
+                                        return message.response(SimpleHttpResponseMessage
+                                                .builder()
+                                                .status(200)
+                                                .contentType(MediaType.APPLICATION_JSON)
+                                                .payload(Unpooled.wrappedBuffer("{\"success\":true}".getBytes()))
+                                                .build())
+                                                .thenReturn(msg);
+                                    });
+                            })
+                        .switchIfEmpty( Mono.defer(() -> {
+                            DeviceRegisterMessage registerMessage = new DeviceRegisterMessage();
+                            registerMessage.addHeader("productId", "004");
+                            registerMessage.addHeader("deviceName", "特种-" + deviceId);
+                            registerMessage.setDeviceId(deviceId);
+                            return Mono.just(registerMessage);
+                        }));
+        });
     }
 
     @NotNull
     @Override
     public Publisher<? extends EncodedMessage> encode(@NotNull MessageEncodeContext messageEncodeContext) {
-        log.info("encode");
+        log.info("CraneDeviceMessageCodec encode");
         return null;
     }
 }
